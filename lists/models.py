8ced7bef858df6b9e40de8dd86866133266f2df7
haldaniko <haldaniko@gmail.com>
2024-11-21 00:12:03 +0100

    add models 'Movie', 'Game', 'Event', 'Idea'

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,31 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    rate = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
+    status = models.BooleanField()
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.BooleanField()
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField()
+
+
+class Idea(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
