5e3665b85cbd1838c9dea998bb94d634dc38db69
haldaniko <haldaniko@gmail.com>
2024-11-25 19:26:56 +0100

    update models 'Movie', 'Game', 'Event', 'Idea'; add models 'Desire', 'Goal', 'SubTask'

--- lists/models.py
+++ lists/models.py
@@ -2,30 +2,113 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
+class StatusChoices(models.TextChoices):
+    PENDING = "pending", "Pending"
+    ACTIVE = "active", "Active"
+    COMPLETED = "completed", "Completed"
+
+
 class Movie(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
+    name = models.CharField(
+        max_length=255
+    )
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
     rate = models.IntegerField(
+        null = True,
         validators=[
             MinValueValidator(1),
             MaxValueValidator(5)
         ]
     )
-    status = models.BooleanField()
+    status = models.CharField(
+        max_length=10,
+        choices=StatusChoices.choices,
+        default=StatusChoices.PENDING.value,
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Game(models.Model):
+    KOOP_CHOICES = [
+        ('solo', 'Solo'),
+        ('double', 'Double'),
+        ('team', 'Team'),
+    ]
+
     name = models.CharField(max_length=255)
-    description = models.TextField()
-    status = models.BooleanField()
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=StatusChoices.choices,
+        default=StatusChoices.PENDING.value,
+    )
+    rate = models.IntegerField(
+        null=True,
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
+    review = models.TextField(
+        blank=True,
+        null=True
+    )
+    koop = models.CharField(
+        max_length=20,
+        choices=KOOP_CHOICES,
+        default='Solo',
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Event(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     date = models.DateField()
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Idea(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(
+        max_length=10,
+        choices=StatusChoices.choices,
+        default=StatusChoices.PENDING.value,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Desire(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.BooleanField(default=False)
+    price = models.FloatField(blank=True, null=True)
+    link = models.URLField(blank=True, null=True)
+
+
+class Goal(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.BooleanField(default=False)
+
+
+class SubTask(models.Model):
+    name = models.CharField(max_length=255)
+    status = models.BooleanField(default=False)
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE)
