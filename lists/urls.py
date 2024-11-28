c5c7c29a69f86881a97cf78af0e7eb2898159c12
haldaniko <haldaniko@gmail.com>
2024-11-28 16:39:17 +0100

    add endpoints for 'Movie' (CRUD)

--- lists/urls.py
+++ lists/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
-from lists.views import index, MovieListView
+from lists.views import (
+    index,
+    MovieListView, MovieCreateView, MovieUpdateView, MovieDeleteView, MovieDetailView
+)
 
 app_name = "lists"
 
@@ -9,11 +12,12 @@ urlpatterns = [
     path('index/', index, name='index'),
     path('', index, name='index'),
 
-    # region ---------- Topic Views  ----------
+    # region ---------- Movie Views  ----------
     path("movies/", MovieListView.as_view(), name="movie-list"),
-    # path("topics/create/", TopicCreateView.as_view(), name="topic-create"),
-    # path("topics/<int:pk>/update/", TopicUpdateView.as_view(), name="topic-update"),
-    # path("topics/<int:pk>/delete/", TopicDeleteView.as_view(), name="topic-delete"),
+    path("movies/create/", MovieCreateView.as_view(), name="movie-create"),
+    path("movies/<int:pk>/", MovieDetailView.as_view(), name="movie-detail"),
+    path("movies/<int:pk>/update/", MovieUpdateView.as_view(), name="movie-update"),
+    path("movies/<int:pk>/delete/", MovieDeleteView.as_view(), name="movie-delete"),
     # endregion ---------- Topic Views  ----------
 
 ]
