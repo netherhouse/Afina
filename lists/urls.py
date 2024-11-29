2b4bf694762257c8e2cd57c88759c239a864b926
haldaniko <haldaniko@gmail.com>
2024-11-29 19:02:38 +0100

    register 'CRUD' endpoints for model 'Game'

--- lists/urls.py
+++ lists/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from lists.views import (
     index,
-    MovieListView, MovieCreateView, MovieUpdateView, MovieDeleteView, MovieDetailView
+    MovieListView, MovieCreateView, MovieUpdateView, MovieDeleteView, MovieDetailView,
+    GameListView, GameCreateView, GameDetailView, GameUpdateView, GameDeleteView,
+
 )
 
 app_name = "lists"
-
 
 urlpatterns = [
     path('index/', index, name='index'),
@@ -19,5 +20,12 @@ urlpatterns = [
     path("movies/<int:pk>/update/", MovieUpdateView.as_view(), name="movie-update"),
     path("movies/<int:pk>/delete/", MovieDeleteView.as_view(), name="movie-delete"),
     # endregion ---------- Topic Views  ----------
+    # region ---------- Game Views  ----------
+    path("games/", GameListView.as_view(), name="game-list"),
+    path("games/create/", GameCreateView.as_view(), name="game-create"),
+    path("games/<int:pk>/", GameDetailView.as_view(), name="game-detail"),
+    path("games/<int:pk>/update/", GameUpdateView.as_view(), name="game-update"),
+    path("games/<int:pk>/delete/", GameDeleteView.as_view(), name="game-delete"),
+    # endregion ---------- Game Views  ----------
 
 ]
