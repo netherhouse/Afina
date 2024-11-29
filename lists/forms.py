0ddb559cbd5e4872c2b36dfe0c9a1f118e1b3fd5
haldaniko <haldaniko@gmail.com>
2024-11-29 19:33:42 +0100

    rename 'MovieSearchForm' to 'NameSearchForm'

--- lists/forms.py
+++ lists/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 
-class MovieSearchForm(forms.Form):
+class NameSearchForm(forms.Form):
     name = forms.CharField(
         max_length=255,
         required=False,
