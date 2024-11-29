54269f2c06755ec25cc937318dddb8497e01c575
haldaniko <haldaniko@gmail.com>
2024-11-29 19:02:25 +0100

    add 'CRUD' views for model 'Game'

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,9 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
 from lists.forms import MovieSearchForm
-from lists.models import Movie
+from lists.models import Movie, Game
 
 
 def index(request):
@@ -57,4 +56,50 @@ class MovieDeleteView(generic.DeleteView):
     template_name = "lists/movie/movie_confirm_delete.html"
 
 
-# endregion ---------- Topic Views  ----------
+# endregion ---------- Movie Views  ----------
+
+# region ---------- Game Views  ----------
+class GameListView(generic.ListView):
+    model = Game
+    context_object_name = "game_list"
+    template_name = "lists/game/game_list.html"
+    paginate_by = 10
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(GameListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = MovieSearchForm(initial={"name": name})
+        return context
+
+    def get_queryset(self):
+        queryset = Movie.objects.all()
+        form = MovieSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(name__icontains=form.cleaned_data["name"])
+        return queryset
+
+
+class GameCreateView(generic.CreateView):
+    model = Game
+    fields = "__all__"
+    success_url = reverse_lazy("lists:game-list")
+    template_name = "lists/game/game_form.html"
+
+
+class GameDetailView(generic.DetailView):
+    model = Game
+    template_name = "lists/game/game_detail.html"
+
+
+class GameUpdateView(generic.UpdateView):
+    model = Game
+    fields = "__all__"
+    success_url = reverse_lazy("lists:game-list")
+    template_name = "lists/game/game_form.html"
+
+
+class GameDeleteView(generic.DeleteView):
+    model = Game
+    success_url = reverse_lazy("lists:game-list")
+    template_name = "lists/game/game_confirm_delete.html"
+# endregion ---------- Game Views  ----------
