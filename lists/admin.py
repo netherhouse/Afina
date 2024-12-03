be979e2cd57799be243c60a26fc7cfe820a2cce7
haldaniko <haldaniko@gmail.com>
2024-12-03 01:17:59 +0100

    register 'Idea' in admin.py+

--- lists/admin.py
+++ lists/admin.py
@@ -5,6 +5,7 @@ from lists import models
 admin.site.register(models.Movie)
 admin.site.register(models.Game)
 admin.site.register(models.Event)
+admin.site.register(models.Idea)
 admin.site.register(models.Desire)
 admin.site.register(models.Goal)
 admin.site.register(models.SubTask)
