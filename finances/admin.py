f4e63c67af1fd07d0f71fdf53026230a9a364118
haldaniko <haldaniko@gmail.com>
2024-12-03 20:15:34 +0100

    register 'WalletAdmin', 'TransactionCategoryAdmin' and 'TransactionAdmin' in admin.py

--- finances/admin.py
+++ finances/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Wallet, Transaction, TransactionCategory
 
-# Register your models here.
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ("name", "currency", "balance", "wallet_type")
+    search_fields = ("name", "currency")
+
+
+@admin.register(TransactionCategory)
+class TransactionCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+    search_fields = ("name",)
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ("wallet", "transaction_type", "amount", "date", "comment")
+    search_fields = ("wallet__name", "comment")
+    list_filter = ("transaction_type", "date")
