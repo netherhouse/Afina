1120aae5b1acfa05e707c4e5fe807b08ce5cec17
haldaniko <haldaniko@gmail.com>
2024-12-16 22:43:01 +0100

    add view "wallets" for main "finances" app page

--- finances/views.py
+++ finances/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+from django.shortcuts import render
+from dotenv import load_dotenv
+
+from finances.services.monobank import MonoBankAPI
+
+load_dotenv()
+mono_token = os.getenv("monobankToken")
+iban_list = os.getenv("IBANs").split(",")
+bank_api = MonoBankAPI(mono_token=mono_token, iban_list=iban_list)
+
+
+def wallets(request):
+
+    monobank_wallets = bank_api.get_wallets()
+
+    if monobank_wallets is None:
+        monobank_wallets = {}
+    return render(request, 'finances/wallets.html', {'monobank_wallets': monobank_wallets})
