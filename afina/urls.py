aac0d88a2798db8f99a630df2c70b63531a64587
haldaniko <haldaniko@gmail.com>
2024-12-17 20:35:03 +0100

    move "index" endpoint from "/lists/urls.py" to "/afina/urls.py"

--- afina/urls.py
+++ afina/urls.py
@@ -3,7 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from lists.views import index
+
 urlpatterns = [
+    path('', index, name='index'),
     path("admin/", admin.site.urls),
     path("lists/", include("lists.urls", namespace="lists")),
     path("finances/", include("finances.urls", namespace="finances")),
--- lists/urls.py
+++ lists/urls.py
@@ -13,9 +13,6 @@ app_name = "lists"
 
 
 urlpatterns = [
-    path('index/', index, name='index'),
-    path('', index, name='index'),
-
     # region ---------- Movie Views  ----------
     path("movies/", MovieListView.as_view(), name="movie-list"),
     path("movies/create/", MovieCreateView.as_view(), name="movie-create"),
