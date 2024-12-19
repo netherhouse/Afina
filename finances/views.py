802bdea6ac67b9a73b79d2f8ee7693dd9dfdd862
haldaniko <haldaniko@gmail.com>
2024-12-19 21:12:38 +0100

    add 'nbu_rates' to 'wallets' conetxt

--- finances/views.py
+++ finances/views.py
@@ -3,18 +3,29 @@ import os
 from django.shortcuts import render
 from dotenv import load_dotenv
 
-from finances.services.monobank import MonoBankAPI
+from finances.services.monobank import MonoBankAPI, NBUAPI
 
 load_dotenv()
 mono_token = os.getenv("monobankToken")
 iban_list = os.getenv("IBANs").split(",")
 bank_api = MonoBankAPI(mono_token=mono_token, iban_list=iban_list)
+nbu_api = NBUAPI()
 
 
 def wallets(request):
 
     monobank_wallets = bank_api.get_wallets()
+    nbu_rates = nbu_api.get_rates()
 
     if monobank_wallets is None:
         monobank_wallets = {}
-    return render(request, 'finances/wallets.html', {'monobank_wallets': monobank_wallets})
+    if nbu_rates is None:
+        nbu_rates = {}
+
+    print(nbu_rates)
+
+    return render(
+        request,
+        'finances/wallets.html',
+        {'monobank_wallets': monobank_wallets, "nbu_rates": nbu_rates}
+    )
