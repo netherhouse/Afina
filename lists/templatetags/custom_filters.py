5212ac3dff3f5a95897b91d7f72f1b01185e6a4c
haldaniko <haldaniko@gmail.com>
2024-12-25 23:32:05 +0100

    change 'num_words' param in 'truncate_words' to 9

--- lists/templatetags/custom_filters.py
+++ lists/templatetags/custom_filters.py
@@ -4,6 +4,6 @@ register = template.Library()
 
 
 @register.filter
-def truncate_words(value, num_words=6):
+def truncate_words(value, num_words=9):
     words = value.split()
     return ' '.join(words[:num_words]) + ('...' if len(words) > num_words else '')
