82772f06191fe1ccd93bab0ee8372fff1673c024
haldaniko <haldaniko@gmail.com>
2025-03-15 17:28:25 +0200

    update routing in 'urls.py'

--- afina/urls.py
+++ afina/urls.py
@@ -6,10 +6,10 @@ from django.conf.urls.static import static
 from lists.views import index
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path('afina/', index, name='index'),
-    path("admin/", admin.site.urls),
-    path("lists/", include("lists.urls", namespace="lists")),
-    path("finances/", include("finances.urls", namespace="finances")),
+    path("afina/lists/", include("lists.urls", namespace="lists")),
+    path("afina/finances/", include("finances.urls", namespace="finances")),
     path("", include("website.urls", namespace="website")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
