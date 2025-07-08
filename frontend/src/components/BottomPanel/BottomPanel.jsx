bf531575d3c1120ed503b9621cc30aca2ad57318
haldaniko <haldaniko@gmail.com>
2025-07-08 16:08:50 +0300

    update BottomPanel

--- frontend/src/components/BottomPanel/BottomPanel.jsx
+++ frontend/src/components/BottomPanel/BottomPanel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './BottomPanel.css';
 import playIconPath from '../../assets/buttons/play.svg';
 import playnextIconPath from '../../assets/buttons/play-next.svg';
@@ -9,24 +9,26 @@ import clockIconPath from '../../assets/buttons/clock.svg';
 import { UseFullscreen } from './useFullscreen';
 import ShowTime from './showTime.jsx';
 
-
-
-function BottomPanel({ openWindow }) {
+function BottomPanel({ openWindow, activeWindows }) {
   const { isFullscreen, toggleFullscreen } = UseFullscreen();
 
   return (
     <div className="bottom-panel">
       <div className="bottom-panel__icons">
-        <div className="bottom-panel__icon-button"
-             onClick={() => openWindow('music')}>
+        <div
+          className={`bottom-panel__icon-button ${activeWindows.includes('music') ? 'active' : ''}`}
+          onClick={() => openWindow('music')}
+        >
           <img src={playIconPath} alt="Play music" width={18} height={18} />
         </div>
         <div className="bottom-panel__icon-button">
           <img src={playnextIconPath} alt="Next Song" width={18} height={18} />
         </div>
         <div className="bottom-panel__divider"></div>
-        <div className="bottom-panel__icon-button"
-             onClick={() => openWindow('pomodoro')}>
+        <div
+          className={`bottom-panel__icon-button ${activeWindows.includes('pomodoro') ? 'active' : ''}`}
+          onClick={() => openWindow('pomodoro')}
+        >
           <img src={clockIconPath} alt="Clock" width={16} height={16} />
         </div>
       </div>
@@ -34,12 +36,13 @@ function BottomPanel({ openWindow }) {
       <div className="bottom-panel__icons">
         <ShowTime />
         <div className="bottom-panel__divider"></div>
-        <div className="bottom-panel__icon-button"
-             onClick={() => openWindow('user')}>
+        <div
+          className={`bottom-panel__icon-button ${activeWindows.includes('user') ? 'active' : ''}`}
+          onClick={() => openWindow('user')}
+        >
           <img src={userIconPath} alt="Profile" width={18} height={18} />
         </div>
-        <div className="bottom-panel__icon-button"
-             onClick={toggleFullscreen}>
+        <div className="bottom-panel__icon-button" onClick={toggleFullscreen}>
           <img
             src={isFullscreen ? exitFullscreenIconPath : fullscreenIconPath}
             alt={isFullscreen ? 'Exit Full Screen' : 'Full Screen'}
