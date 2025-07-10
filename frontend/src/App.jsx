5ff98def4e562ea87c40db3d1de39ed672f261b7
netherhouse <nether3ord@gmail.com>
2025-07-10 15:05:01 +0200

    feat: implement active window management in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import VideoBackground from "./components/VideoBackground/VideoBackground";
 import BottomPanel from "./components/BottomPanel/BottomPanel";
 import WindowManager from "./components/Window/WindowManager";
@@ -6,14 +6,26 @@ import "./App.scss";
 
 function App() {
   const windowManagerRef = useRef(null);
+  const [activeWindows, setActiveWindows] = useState([]);
+  const [activeWindow, setActiveWindow] = useState(null);
+
+  const handleActiveWindowsChange = (windows, focusedWindow) => {
+    setActiveWindows(windows);
+    setActiveWindow(focusedWindow);
+  };
 
   return (
     <div className="App">
       <VideoBackground />
       <BottomPanel
         openWindow={(id) => windowManagerRef.current?.openWindow(id)}
+        activeWindows={activeWindows}
+        activeWindow={activeWindow}
       />
-      <WindowManager ref={windowManagerRef} />
+      <WindowManager
+        ref={windowManagerRef}
+        onActiveWindowsChange={handleActiveWindowsChange}
+      />
     </div>
   );
 }
