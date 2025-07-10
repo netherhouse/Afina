c0125d8e847f2c19a341649d7396e4658beffd32
haldaniko <haldaniko@gmail.com>
2025-07-10 15:23:08 +0300

    update BottomPanel content and styles

--- frontend/src/components/BottomPanel/BottomPanel.jsx
+++ frontend/src/components/BottomPanel/BottomPanel.jsx
@@ -1,11 +1,14 @@
 // import React, { useState, useEffect } from "react";
 import "./BottomPanel.scss";
+
 import playIconPath from "../../assets/buttons/play.svg";
 import playnextIconPath from "../../assets/buttons/play-next.svg";
 import fullscreenIconPath from "../../assets/buttons/full-screen.svg";
 import exitFullscreenIconPath from "../../assets/buttons/fullscreen-exit.svg";
 import userIconPath from "../../assets/buttons/user.svg";
 import clockIconPath from "../../assets/buttons/clock.svg";
+import mixerIconPath from "../../assets/buttons/mixer.svg";
+
 import { UseFullscreen } from "./useFullscreen";
 import ShowTime from "./showTime.jsx";
 
@@ -15,16 +18,20 @@ function BottomPanel({ openWindow, activeWindows }) {
   return (
     <div className="bottom-panel">
       <div className="bottom-panel__icons">
+        <div className="bottom-panel__icon-button">
+          <img src={playIconPath} alt="Next Song" width={18} height={18} />
+        </div>
+        <div className="bottom-panel__icon-button">
+          <img src={playnextIconPath} alt="Next Song" width={18} height={18} />
+        </div>
+
         <div
           className={`bottom-panel__icon-button ${
             activeWindows?.includes("music") ? "active" : ""
           }`}
           onClick={() => openWindow("music")}
         >
-          <img src={playIconPath} alt="Play music" width={18} height={18} />
-        </div>
-        <div className="bottom-panel__icon-button">
-          <img src={playnextIconPath} alt="Next Song" width={18} height={18} />
+          <img src={mixerIconPath} alt="Play music" width={18} height={18} />
         </div>
         <div className="bottom-panel__divider"></div>
         <div
